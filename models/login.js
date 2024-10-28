71701d5153b380c86b9f209f6691bbbe058e74e5
guilherme-paiva1 <guilherme.paiva@sptech.school>
2024-10-28 14:33:40 -0300

    criação do modal de login e cadastro e estilizações

---
+++ models/login.js
@@ -0,0 +1,10 @@
+function exibirCadastro() {
+    header_login.classList.add('unactive');
+    header_cadastro.classList.remove('unactive');
+
+    body_login.classList.add('unactive');
+    body_cadastro.classList.remove('unactive');
+
+    footer_login.classList.add('unactive');
+    footer_cadastro.classList.remove('unactive');
+}
