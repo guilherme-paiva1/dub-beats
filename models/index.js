ba295f1d03e0c15ee5a1adcbe6f550119282f793
guilherme-paiva1 <guilherme.paiva@sptech.school>
2024-10-28 19:56:41 -0300

    finalização de navbar

--- models/index.js
+++ models/index.js
@@ -8,3 +8,14 @@ function exibirCadastro() {
     footer_login.classList.add('unactive');
     footer_cadastro.classList.remove('unactive');
 }
+
+function exibirLogin() {
+    header_cadastro.classList.add('unactive');
+    header_login.classList.remove('unactive');
+
+    body_cadastro.classList.add('unactive');
+    body_login.classList.remove('unactive');
+
+    footer_cadastro.classList.add('unactive');
+    footer_login.classList.remove('unactive');
+}
