9e791550428499f1f45840439c4be6a73f87356c
Guilherme Vieira <guilherme.paiva@sptech.school>
2024-11-03 09:18:38 -0300

    criação da modelagem e primeira versão do script do banco de dados

---
+++ database/dub_beats.sql
@@ -0,0 +1,70 @@
+
+USE dub_beats;
+
+CREATE TABLE Usuario (
+  id_usuario INT PRIMARY KEY AUTO_INCREMENT ,
+  nome VARCHAR(45),
+  email VARCHAR(45),
+  senha VARCHAR(45)
+);
+
+CREATE TABLE Postagem (
+  id_postagem INT AUTO_INCREMENT,
+  fk_usuario INT,
+  titulo VARCHAR(45),
+  conteudo VARCHAR(500),
+  PRIMARY KEY (id_postagem, fk_usuario),
+
+  CONSTRAINT fk_Postagem_Usuario
+    FOREIGN KEY (fk_usuario)
+		REFERENCES Usuario(id_usuario)
+);
+
+
+CREATE TABLE Comentario (
+	id_comentario INT AUTO_INCREMENT,
+	fk_postagem INT
+	fk_usuario INT,
+	conteudo VARCHAR(500),
+	PRIMARY KEY (id_comentario, fk_postagem, fk_usuario),
+
+	CONSTRAINT fk_postagem_comentario
+		FOREIGN KEY (fk_postagem)
+			REFERENCES Postagem(id_postagem),
+	CONSTRAINT fk_usuario_comentario
+		FOREIGN KEY (fk_usuario)
+			REFERENCES Postagem(fk_usuario)
+);
+
+CREATE TABLE Curtida_Postagem (
+  id_curtida_postagem INT AUTO_INCREMENT,
+  fk_postagem INT,
+  fk_usuario INT,
+  PRIMARY KEY (id_curtida_postagem, fk_postagem, fk_usuario),
+
+  CONSTRAINT fk_postagem_curtida
+    FOREIGN KEY (fk_postagem)
+		REFERENCES Postagem (id_postagem),
+   CONSTRAINT fk_usuario_postagem_curtida
+    FOREIGN KEY (fk_usuario)
+		REFERENCES Postagem(fk_usuario)
+);
+
+CREATE TABLE Curtida_Comentario (
+  id_curtida_comentario INT AUTO_INCREMENT,
+  fk_comentario INT,
+  fk_postagem INT,
+  fk_usuario INT,
+  PRIMARY KEY (id_curtida_comentario, fk_comentario, fk_postagem, fk_usuario),
+
+  CONSTRAINT fk_comentario_curtida
+    FOREIGN KEY (fk_comentario)
+		REFERENCES Comentario (id_comentario),
+  CONSTRAINT fk_postagem_comentario_curtida
+    FOREIGN KEY (fk_postagem)
+		REFERENCES Comentario (fk_postagem),
+  CONSTRAINT fk_usuario_comentario_curtida
+    FOREIGN KEY (fk_usuario)
+		REFERENCES Comentario (fk_usuario)
+);
+
--- site/public/js/index.js
+++
@@ -1,9 +0,0 @@
-var nav_home = document.getElementById('nav_home');
-if (nav_home) {
-    nav_home.classList.add('active');
-}
-
-var nav_sobre = document.getElementById('nav_sobre');
-if (nav_sobre) {
-    nav_sobre.classList.remove('active');
-}
